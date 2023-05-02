bf9224736a9937d1998c6b6030e83f8c92e47c67
khokons <khokon.web9226@gmail.com>
2023-05-02 08:40:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 14, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+
+  document.getElementById("days").innerHTML = days
+  document.getElementById("hours").innerHTML = hours
+  document.getElementById("minutes").innerHTML = minutes
+  document.getElementById("seconds").innerHTML = seconds
+
+
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+$('.test_slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+  nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+
+
+
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+
+}); 
+
+
+
+
+
+
